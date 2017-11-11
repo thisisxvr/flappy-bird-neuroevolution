32814dc30263fb0030dcdc63b3cdb400406c15d9
Xavier Francis <thisisxvr@gmail.com>
2017-11-11 14:00:38 +1300

    Generation defined

--- genetic-algorithm.ts
+++ genetic-algorithm.ts
@@ -2,28 +2,48 @@ import NeuralNetwork from "./neural-network"
 
 namespace GeneticAlgorithm {
 
-  const mutationRate = 0.1
-  const mutationRange = 0.5
-  const crossoverRate = 0.5
+  const crossoverRate  = 0.5
+  const elitismRate    = 0.2
+  const mutationRate   = 0.1
+  const mutationRange  = 0.5
+  const randomnessRate = 0.2
+  let populationCount: number
   let generations: Generation[]
 
-  function evolve() {
+  function evolve(networkShape = [2, [2], 1], popCount = 50): Generation {
+    populationCount = popCount
     if (generations.length === 0) {
-      //
+      generations = []
+      const nextGeneration = new Generation(networkShape, populationCount)
+      generations.push(nextGeneration)
+      return nextGeneration
+    } else {
+      const currentGeneration = generations[generations.length - 1]
+      const nextGeneration = currentGeneration.next()
+      generations.push(nextGeneration)
+      return nextGeneration
     }
   }
 
   class Generation {
-    private population: NeuralNetwork[]
+    private _population: NeuralNetwork[]
 
-    constructor(populationCount: number) {
-      for (let i = 0; i < populationCount; i++) {
-        this.population[i] = new NeuralNetwork()
+    constructor(networkShape = [2, [2], 1], popCount = populationCount, newPopulation?: NeuralNetwork[]) {
+      if (newPopulation) {
+        this._population = this.sortPopulation(newPopulation)
+        return
+      }
+
+      this._population = new Array<NeuralNetwork>(popCount)
+
+      for (let i = 0; i < popCount; i++) {
+        this._population[i] = new NeuralNetwork(networkShape)
       }
     }
 
-    sortPopulation() {
-      const mapped = this.population.map((chromosome, index) => {
+    // Sorts the population in descending order, ranked by score.
+    private sortPopulation(population = this._population): NeuralNetwork[] {
+      const mapped = population.map((chromosome, index) => {
         return { index, score: chromosome.fitness }
       })
 
@@ -33,11 +53,11 @@ namespace GeneticAlgorithm {
         return 0
       })
 
-      const temp = mapped.map((chromosome) => { return this.population[chromosome.index] })
-      this.population = temp
+      return mapped.map((chromosome) => { return population[chromosome.index] })
     }
 
-    breed(parentOne: NeuralNetwork, parentTwo: NeuralNetwork, numberOfOffspring: number): NeuralNetwork[] {
+    // Takes a pair of parents, performs crossover and mutation and returns the offspring.
+    private breed(parentOne: NeuralNetwork, parentTwo: NeuralNetwork, numberOfOffspring = 1): NeuralNetwork[] {
       function crossover(parentOne: NeuralNetwork, parentTwo: NeuralNetwork): NeuralNetwork {
         const child = parentOne
 
@@ -67,6 +87,45 @@ namespace GeneticAlgorithm {
       return offspring
     }
 
+    // Returns the next generation.
+    next(): Generation {
+      const nextGen = []
+      this.sortPopulation()
+
+      // Push some of the elite chromosomes into the next generation unchanged.
+      for (let i = 0; i < Math.round(elitismRate * populationCount); i++) {
+        if (nextGen.length < populationCount) { nextGen.push(this._population[i]) }
+      }
+
+      // Introduce some randomness into the next generation.
+      for (let i = 0; i < Math.round(randomnessRate * populationCount); i++) {
+        const firstNetwork = this._population[0]
+        for (const i in firstNetwork.weights) {
+          if (nextGen.length < populationCount) { nextGen.push(firstNetwork.weights[i] = this.randomClamped()) }
+        }
+      }
+
+      // Breed new children from the elites until we hit the population limit.
+      // let ceiling = 1
+      for (let ceiling = 1; ceiling < populationCount; ceiling++) {
+        for (let i = 0; i < ceiling; i++) {
+          const children = this.breed(this._population[i], this._population[ceiling])
+          for (const child of children) { nextGen.push(child) }
+          if (nextGen.length >= populationCount) { break }
+        }
+        if (nextGen.length >= populationCount) { break }
+        // ceiling++
+        // if (ceiling >= this._population.length - 1) { ceiling = 0 }
+      }
+
+      return new Generation(undefined, undefined, nextGen as NeuralNetwork[])
+    }
+
+    // Returns a random value between -1 and 1.
+    private randomClamped(): number { return Math.random() * 2 - 1 }
+
   }
+
 }
+
 export default GeneticAlgorithm
