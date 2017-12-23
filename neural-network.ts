277ee975b2d8a21f78dcbe7a5aced664e0db33df
Xavier Francis <thisisxvr@gmail.com>
2017-12-23 21:09:45 +1300

    Refactor neural net
    - Weights accessor returns an IData object
    - Persist now works with IData type
    - Remove dead code

--- neural-network.ts
+++ neural-network.ts
@@ -1,5 +1,5 @@
 namespace Network {
-  interface IData {
+  export interface IData {
     neurons: number[], // Number of Neurons per layer.
     weights: number[]  // Weights of each Neuron's inputs.
   }
@@ -8,124 +8,62 @@ namespace Network {
     public fitness: number
     private _weights: number[]
     private layers: Layer[]
-    private _inputNodeCount: number
-    private _hiddenLayerCount: number[]
-    private _outputNodeCount: number
 
     constructor([inputNodeCount, ...otherLayers]: Array<number | number[]> = [1, [2], 1]) {
-      this._inputNodeCount = inputNodeCount as number
-      this._outputNodeCount = otherLayers.pop() as number
-      this._hiddenLayerCount = otherLayers[0] as number[]
+      // this._inputNodeCount = inputNodeCount as number
+      const outputNodeCount = otherLayers.pop() as number
+      const hiddenLayerCount = otherLayers[0] as number[]
       this.layers = [], this._weights = []
       let nodesInPreviousLayer = 0, index = 0
+
       // Input layer.
       this.layers[0] = new Layer(inputNodeCount as number, nodesInPreviousLayer, index)
       nodesInPreviousLayer = inputNodeCount as number
+
       // Hidden layers.
-      for (const i in this._hiddenLayerCount as number[]) {
-        const nodeCount = this._hiddenLayerCount[i]
+      for (const i in hiddenLayerCount as number[]) {
+        const nodeCount = hiddenLayerCount[i]
         this.layers.push(new Layer(nodeCount, nodesInPreviousLayer, ++index))
-        nodesInPreviousLayer = this._hiddenLayerCount[i]
+        nodesInPreviousLayer = hiddenLayerCount[i]
       }
+
       // Output layer.
-      this.layers.push(new Layer(this._outputNodeCount as number, nodesInPreviousLayer, ++index))
+      this.layers.push(new Layer(outputNodeCount as number, nodesInPreviousLayer, ++index))
     }
 
     // Returns a flat array with weights of ALL neurons in the network.
     get weights() {
-      if (this._weights.length > 0) { return this._weights }
+      const data: IData = { neurons: [], weights: [] }
+
+      // if (this._weights.length > 0) { return this._weights }
       for (const layer of this.layers) {
+        data.neurons.push(layer.nodes.length)
         for (const i in layer.nodes) {
           this._weights = this._weights.concat(layer.nodes[i].weights)
         }
       }
-      return this._weights
-    }
 
-    getSave() {
-      const datas: IData = { neurons: [], weights: [] }
-
-      // tslint:disable-next-line:no-invalid-this
-      for (const i in this.layers) {
-        datas.neurons.push(this.layers[i].nodes.length)
-        for (const j in this.layers[i].nodes) {
-          for (const k in this.layers[i].nodes[j].weights) {
-              // push all input weights of each Neuron of each Layer into a flat
-              // array.
-            datas.weights.push(this.layers[i].nodes[j].weights[k])
-          }
-        }
-      }
-      return datas
-    }
-
-    setSave(save: IData) {
-      let previousNeurons = 0
-      let index = 0
-      let indexWeights = 0
-      this.layers = []
-      for (const i in save.neurons) {
-        // Create and populate layers.
-        const layer = new Layer(save.neurons[i], previousNeurons, index)
-        for (const j in layer.nodes) {
-          for (const k in layer.nodes[j].weights) {
-              // Apply neurons weights to each Neuron.
-            layer.nodes[j].weights[k] = save.weights[indexWeights]
-
-            indexWeights++ // Increment index of flat array.
-          }
-        }
-        previousNeurons = save.neurons[i]
-        index++
-        this.layers.push(layer)
-      }
+      data.weights = this._weights
+      return data
     }
 
     // Persists the mutated weights to the neurons.
-    persist() {
+    persist(data: IData) {
       // tslint:disable-next-line:prefer-for-of
+      let nodesInPreviousLayer = 0
       let index = 0
-      // for (const layer of this.layers) {
-      //   const nodeCount = layer.nodes.length
-      //   const
-      //   for (const i in layer.nodes) {
-      //     const node = layer.nodes[i]
-      //     node.value = undefined!
-      //     for (const j in node.weights) {
-      //       node.weights[j] = this._weights[index]
-      //       index++
-      //     }
-      //   }
-      // }
-      let nodesInPreviousLayer = 0, layerID = 0
-      // Input layer.
-      this.layers[0] = new Layer(this._inputNodeCount as number, nodesInPreviousLayer, layerID)
-      nodesInPreviousLayer = this._inputNodeCount as number
-      // Hidden layers.
-      for (const i in this._hiddenLayerCount as number[]) {
-        const nodeCount = this._hiddenLayerCount[i]
-        const layer = new Layer(nodeCount, nodesInPreviousLayer, ++layerID)
+      this.layers = []
+      for (const i in data.neurons) {
+        const layer = new Layer(data.neurons[i], nodesInPreviousLayer)
         for (const j in layer.nodes) {
           const node = layer.nodes[j]
           for (const k in node.weights) {
-            node.weights[k] = this._weights[index]
-            index++
+            node.weights[k] = data.weights[index++]
           }
         }
-        this.layers[1] = layer
-        nodesInPreviousLayer = this._hiddenLayerCount[i]
+        nodesInPreviousLayer = data.neurons[i]
+        this.layers.push(layer)
       }
-      // Output layer.
-      const layer = new Layer(this._outputNodeCount as number, nodesInPreviousLayer, ++layerID)
-      for (const i in layer.nodes) {
-        const node = layer.nodes[i]
-        for (const j in node.weights) {
-          node.weights[j] = this._weights[index]
-          index++
-        }
-      }
-      this.layers[2] = layer
-      this._weights = []
     }
 
     // Computes the output of the network.
@@ -141,13 +79,7 @@ namespace Network {
       let previousLayer = inputLayer
       for (let i = 1; i < this.layers.length; i++) {
         const layer = this.layers[i]
-        // for (const neuron of layer) {
-        //   for (const previousNeuron of previousLayer) {
-        //     let sum = 0
-        //     for (const weight of neuron.weights) { sum += previousNeuron.value * weight }
-        //     neuron.activate(sum)
-        //   }
-        // }
+
         for (const j in layer.nodes) {
           let sum = 0
           for (const k in previousLayer.nodes) {
@@ -155,6 +87,7 @@ namespace Network {
           }
           layer.nodes[j].activate(sum)
         }
+
         previousLayer = layer
       }
 
@@ -167,13 +100,13 @@ namespace Network {
   }
 
   class Layer {
-    private id: number
+    // private id: number
     private _nodes: Neuron[]
     private pointer = 0
     get nodes() { return this._nodes }
 
-    constructor(nodeCount: number, inputCount: number, index = 0) {
-      this.id = index
+    constructor(nodeCount: number, inputCount: number, _ = 0) {
+      // this.id = index
       this._nodes = new Array<Neuron>(nodeCount)
       for (let i = 0; i < nodeCount; i++) { this._nodes[i] = new Neuron(inputCount) }
     }
@@ -188,7 +121,7 @@ namespace Network {
     }
 
     // Allows for iterating over a Layer object.
-    // [Symbol.iterator](): IterableIterator<Neuron> { return this }
+    [Symbol.iterator](): IterableIterator<Neuron> { return this }
   }
 
   class Neuron {
@@ -198,8 +131,11 @@ namespace Network {
     constructor(weightCount: number) {
       this.value = undefined!
       this.weights = new Array(weightCount)
-      for (let i = 0; i < weightCount; i++) { this.weights[i] = Math.random() * 2 - 1 }
+      for (let i = 0; i < weightCount; i++) { this.weights[i] = this.randomClamped() }
     }
+
+    /** Returns a random value between -1 and 1. */
+    private randomClamped(): number { return Math.random() * 2 - 1 }
 
     // Sigmoid activation function.
     activate(sum: number): number {
